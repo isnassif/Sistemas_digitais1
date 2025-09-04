cca9955c859c7d5090810c53dfc809cf18c9a378
isnassif <ikhlasnassif@gmail.com>
2025-09-04 14:23:48 -0300

    Update mainvga.v

--- lab2/mainvga.v
+++ lab2/mainvga.v
@@ -3,34 +3,35 @@ module main (
     input clk_50MHz,
     input [9:0] SW,
     output [9:0] LEDR,
-	 output [9:0] next_x,
+	output [9:0] next_x,
     output[9:0] next_y,
     output hsyncm,
     output vsyncm,
     output [7:0] redm,
     output [7:0] greenm,
     output [7:0] bluem,
-	 output blank,
-	 output sync,
-	 output clks
+	output blank,
+	output sync,
+	output clks
 );
+	wire fio = next_y*320 +next_x;
+	wire [7:0] c;
 	
 	reg clk_vga = 0;
 	
-	always@(posedge clk_50MHz) begin
-	
+	always@(posedge clk_50MHz) begin	
 		clk_vga <= ~clk_vga;
 	end	
 	
 	vga_driver draw  ( 
-						  .clock(clk_vga),        // 25 MHz PLL
+					.clock(clk_vga),        // 25 MHz PLL
                     .reset(vga_reset),      // Active high reset, manipulated by instantiating module
                     .color_in(color), // Pixel color (RRRGGGBB) for pixel being drawn
                     .hsync(hsyncm),         // All of the connections to the VGA screen below
                     .vsync(vsyncm),
                     .red(redm),
-						  .next_x(next_x),
-						  .next_y(next_y),
+					.next_x(next_x),
+					.next_y(next_y),
                     .green(greenm),
                     .blue(bluem),
                     .sync(sync),
@@ -39,9 +40,9 @@ module main (
 );
 
 	memory draw(
-		.adress(),
+		.adress(fio),
 		.clock(clk_vga),
-		.q()
+		.q(c)
 		
 	);
 	
